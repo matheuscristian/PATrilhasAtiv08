7034bbad067dffc8f80b76b6ffe51dcc8cf9de66
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-02-11 10:46:39 -0300

    Create e.cs

---
+++ e.cs
@@ -0,0 +1,20 @@
+// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+static string Join(string[] l) {
+    return string.Join(" ", l);
+
+}
+
+var l = new List<string>();
+
+while (true) {
+    string e = Console.ReadLine().Trim();
+    if (e == "") {
+        break;
+    }
+
+    l.Add(e);
+}
+
+Console.WriteLine(Join(l.ToArray()));

c4e5bbfee132a04e2392771f60b7562e58642820
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-02-11 10:34:09 -0300

    Create b.cs

---
+++ b.cs
@@ -0,0 +1,7 @@
+// See https://aka.ms/new-console-template for more information
+static int length_of_string(string s) {
+    return s.Length;
+}
+
+string str = Console.ReadLine();
+Console.WriteLine(length_of_string(str));

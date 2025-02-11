0d2e4ecc0299c059ee1377742d78bab22367d23a
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-02-11 10:40:49 -0300

    Create d.cs

---
+++ d.cs
@@ -0,0 +1,16 @@
+// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+static int Max(int[] l) {
+    int max = int.MinValue;
+    foreach (var num in l) {
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    return max;
+}
+
+var numbers = new List<int>() { 1, 2, 3, 4, 5, 1000, 2000, 10, 23, -1 };
+Console.WriteLine(Max(numbers.ToArray()));
